64fc03b506c2049150738f05b1fbc4af8ea371b5
Maxnxi <33512231+Maxnxi@users.noreply.github.com>
2022-07-01 16:58:27 +0300

    Create playground.swift

---
+++ playground.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+func josephusSurvivor(_ n: Int, _ k: Int) -> Int {
+    var someArr: [Int] = Array(1...n)
+    var counter = k
+    
+    while someArr.count > 1 {
+        
+        for i in 0..<someArr.count {
+            
+            counter -= 1
+            
+            if counter == 0 {
+                someArr[i] = 0
+                counter = k
+            }
+        }
+        someArr = someArr.filter { $0 != 0 }
+        print("someArr is : ", someArr)
+        
+    }
+    return someArr.first!
+}
+
+print(josephusSurvivor(7, 3))
+print(josephusSurvivor(11, 19))
+print(josephusSurvivor(300, 300)) // :)
